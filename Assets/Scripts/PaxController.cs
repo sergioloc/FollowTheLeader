04301ab9646f3bf124df88407f9c115a5b84985a
Sergio López Ceballos <sergioloc2@gmail.com>
2020-03-13 21:23:26 +0100

    Jump delay

--- Assets/Scripts/PaxController.cs
+++ Assets/Scripts/PaxController.cs
@@ -6,24 +6,25 @@ public class PaxController : MonoBehaviour
 {
 
 	public int jumpForce = 0;
-	public int speed = 0;
+	public int initialSpeed = 0;
+	public float delay = 0;
 	
 	private Rigidbody2D rb2d;
 	private bool isGround;
+	private int speed;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
 		isGround = true;
+		speed = initialSpeed;
     }
 
     void Update()
     {
-		rb2d.transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-			rb2d.AddForce(Vector2.up * jumpForce * 100);
+			StartCoroutine(Jump(delay));
 		}
     }
 	
@@ -41,5 +42,11 @@ public class PaxController : MonoBehaviour
         {
             isGround = false;
         }
+	}
+	
+	IEnumerator Jump(float d)
+    {
+        yield return new WaitForSeconds(d * 0.1f);
+		rb2d.AddForce(Vector2.up * jumpForce * 100);
 	}
 }
