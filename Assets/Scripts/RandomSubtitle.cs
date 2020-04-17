9d078dc5732d393870dd0d2b4d76c01714d1ca5b
Sergio López Ceballos <sergioloc2@gmail.com>
2020-04-17 17:03:12 +0200

    Random subtitle in main menu

---
+++ Assets/Scripts/RandomSubtitle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSubtitle : MonoBehaviour
+{
+    private Text subtitle;
+    private int rand;
+
+    void Start()
+    {
+        subtitle = GetComponent<Text>();
+        GenerateText();
+    }
+
+    public void GenerateText(){
+        rand = Random.Range(1, 3);
+        if (rand == 1){
+            subtitle.text = "the roof is on fire!";
+        }
+        else {
+            subtitle.text = "Mmmmmmadrid";
+        }
+    }
+}
