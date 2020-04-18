a4121d6b2cc08774a9f61b8f9fbaf2e6231dc53f
Sergio López Ceballos <sergioloc2@gmail.com>
2020-04-18 16:03:09 +0200

    Occlusion culling

---
+++ Assets/Scripts/OcclusionCulling.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OcclusionCulling : MonoBehaviour
+{
+    public GameObject platforms, details, content;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SafeZone")
+        {
+            EnableAll();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "SafeZone")
+        {
+            DisableAll();
+        }
+    }
+
+    private void EnableAll(){
+        platforms.SetActive(true);
+        details.SetActive(true);
+        content.SetActive(true);
+    }
+
+    private void DisableAll(){
+        platforms.SetActive(false);
+        details.SetActive(false);
+        content.SetActive(false);
+    }
+}
