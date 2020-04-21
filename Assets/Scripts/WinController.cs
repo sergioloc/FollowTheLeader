1f50303e4ca2382f9e12386af617c99a970ba3eb
Sergio López Ceballos <sergioloc2@gmail.com>
2020-04-21 19:06:18 +0200

    New winner frame & unlock Sergio

---
+++ Assets/Scripts/WinController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinController : MonoBehaviour
+{
+    public GameObject winFrame;
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.tag == "Player")
+        {
+            StartCoroutine(ShowFrame());
+        }
+    }
+
+    IEnumerator ShowFrame()
+    {
+        yield return new WaitForSeconds(1f);
+		winFrame.SetActive(true);
+        if ((GameValues.difficulty == 3 || GameValues.difficulty == 4) && PlayerPrefs.GetInt("SergioLock") == 0){
+            PlayerPrefs.SetInt("SergioLock", 1);
+            winFrame.GetComponent<Animator>().SetBool("UnlockSergio", true);
+        }
+	}
+
+
+}
